6d7f9336020e1448e2378f3f7dcd61555686e4d1
Felipe P. <fpalmasaldivia@gmail.com>
2026-07-25 14:38:56 -0400

    fix: corregir True -> true en roles.js

--- src/lib/roles.js
+++ src/lib/roles.js
@@ -69,7 +69,7 @@ export const PERMISOS = {
     exportar:          true,
     verHistorial:      'todo',
     gestionarUsuarios: false,
-    editarDatos:       True,
+    editarDatos:       true,
     verAuditoria:      true,
     gestionarClientes: false,
   },
